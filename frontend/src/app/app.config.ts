74ebbadc97ca8cf0911b71df8b837e6b11e2545b
Henry Jimenez <henry.sistemas.jimenez@gmail.com>
2025-06-04 18:19:17 -0500

    add config ts

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+import { firebaseIdTokenInterceptor } from './interceptors/firebase-id-token-interceptor';
+
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), 
+    provideHttpClient(withInterceptors([firebaseIdTokenInterceptor])),    
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
+  ]
+};
